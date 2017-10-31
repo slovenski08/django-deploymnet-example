c36a4fde9dfa5797d2b11c34691907cdf35e2424
slovenski08 <slovenski08@yahoo.com>
2017-10-31 09:05:57 -0500

    'first'

--- learning_templates/learning_templates/settings.py
+++ learning_templates/learning_templates/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '^786)$6f8j4%)mbpb&azs#8cq0m9sb-@or%c+81g)8s&r6d4dp'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'slovenski08.pythonanywhere.com'
+    'slovenski08.pythonanywhere.com',
 ]
 
 
